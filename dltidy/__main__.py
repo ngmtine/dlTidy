c1db01643fc04f57340fb77f7e14b042ca030955
ngmtine <ngmfries@gmail.com>
2023-02-19 22:44:02 +0900

    update

--- dltidy/__main__.py
+++ dltidy/__main__.py
@@ -1,6 +1,26 @@
+import shutil
+import sys
+
+
+def check_executable():
+    executable_list = ("ffmpeg", "AtomicParsley")
+    for i in executable_list:
+        if not shutil.which(i):
+            raise Exception(f"Error: Failed to execute '{i}'")
+    return
+
+
 def main():
-    print("aaa")
+    print("start!")
+
+    try:
+        check_executable()
+
+    except Exception as e:
+        print(e)
+        sys.exit()
 
 
 if __name__ == "__main__":
     main()
+    print("end!")
